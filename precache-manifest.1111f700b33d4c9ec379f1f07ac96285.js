9974ed67467bb27734e549b3b3b72ee15cc3b0df
mackattack3k <marcushanssonmh@gmail.com>
2020-03-25 00:31:03 +0100

    Updates

---
+++ precache-manifest.1111f700b33d4c9ec379f1f07ac96285.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a3b25a2a0de5bf97c077451aae30ecc",
+    "url": "/rand-draw/index.html"
+  },
+  {
+    "revision": "6386982b02fd28fd5d91",
+    "url": "/rand-draw/static/css/main.f4353dc9.chunk.css"
+  },
+  {
+    "revision": "3f8f9b4729fe0eebbb47",
+    "url": "/rand-draw/static/js/2.cbb4ee4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rand-draw/static/js/2.cbb4ee4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6386982b02fd28fd5d91",
+    "url": "/rand-draw/static/js/main.d7ba9f45.chunk.js"
+  },
+  {
+    "revision": "be7759e7b94a629da2c3",
+    "url": "/rand-draw/static/js/runtime-main.37621ec7.js"
+  }
+]);
